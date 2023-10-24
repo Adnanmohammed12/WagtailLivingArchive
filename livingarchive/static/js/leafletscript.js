e2a94d2b1de94d817b3920123d18c293bf02921a
root <root@studentlearn1.cdu.edu.au>
2023-10-24 10:13:23 +0000

    adding more pages

--- livingarchive/static/js/leafletscript.js
+++ livingarchive/static/js/leafletscript.js
@@ -1,72 +1,65 @@
                     // Define the map
                     const map = L.map('map', { attributionControl: false }).setView([51.505, -0.13], 13); // Set your initial coordinates and zoom level
                     // Add the image overlay layer with adjusted bounds for increased width
-                    const imageUrl = 'https://anyimage.io/storage/uploads/e5de371304e8a183f5c88238e0a6ad25';
+	 const imageUrl = 'https://anyimage.io/storage/uploads/e5de371304e8a183f5c88238e0a6ad25';
                     const imageBounds = [[51.53, -0.20], [51.48, -0.06]]; // Adjust the bounds for increased width
                     L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
                     const animalParts = [
         {
-            name: 'Strings1',
+            name: 'Strings',
             coordinates: [
                 [51.524, -0.11], // mouth
 
             ],
-            videoUrl: '/visions-of-a-bushman-the-community-led-participatory-design-of-an-olkola-cultural-knowledge-centre-as-a-research-through-design-project/',
-            imageUrl: '', // Add the URL of the image for this part
-            description: 'Strings1',
+            videoUrl: '/strings',
+            description: 'The Possums made strings which formed the high sandhils separateing the lake from the sea',
         },
         {
-            name: 'Strings2',
+            name: 'Strings',
             coordinates: [
                 [51.524,  -0.078], // mouth
 
             ],
-            videoUrl: '/aboriginal-housing/',
-            imageUrl: '', // Add the URL of the image for this part
-            description: 'Strings2',
+            videoUrl: '/strings',
+            description: 'The lines extending from the base of the the elongated figure represents the gullies in sandhills that were formed by lengths of possum fur laid out by the Guwak for gathering the clans in the area',
         },
         {
-            name: 'Cashew nuts5',
+            name: 'Cashew nuts',
             coordinates: [
                 [51.524, -0.095], // mouth
 
             ],
-            videoUrl: '/acoustics-of-the-yidaki/',
-            imageUrl: 'https://gymbeam.com/blog/wp-content/uploads/fly-images/140329/kesu-orechy-clanok_1_-min-437x279.png', // Add the URL of the image for this part
-            description: 'Cashew nuts have positive effects on our body across our entire body. Despite the fact that half of their composition is fat, they help us to lose weight. At the same time, they prevent the formation of gallstones and scientific studies have confirmed that they are an effective fighter against digestive tract cancer. Learn why cashews can rightly be called a superfood..',
+            videoUrl: '/cashew-nuts/',
+            description: 'Cashew nuts have positive effects on our body across our entire body. Despite the fact that half of their composition is fat, they help us to lose weight. At the same time, they prevent the formation of gallstones and scientific studies have confirmed that they are an effective fighter against digestive tract cancer. Learn why cashews can rightly be called a superfood.',
         },        
 {
             name: 'Guwak',
             coordinates: [
-                [51.524, -0.159], // mouth
+                [51.524, -0.159], //cashews 
 
             ],
-            videoUrl: '/painting-by-payi-cables-technology-and-engineering-april-2022/',
-            imageUrl: '', // Add the URL of the image for this part
-            description: 'Guwak',
+            videoUrl: 'https://livingarchive.teachingforchange.edu.au/guwak-bird',
+            description: 'The Guwak flew into the casuarina trees, signified by the nuts in the top half of the painting',
         },
         ,
         {
-            name: 'posan1',
+            name: 'Possum',
             coordinates: [
                 [51.509, -0.08], // mouth
 
             ],
-            videoUrl: '/appropriate-technology/',
-            imageUrl: '', // Add the URL of the image for this part
-            description: 'posan',
+            videoUrl: '/possum/',
+            description: 'The Guwak instructed their workers to climb up the tree, spinning their fur into lengths of string',
         },
         ,
         {
-            name: 'Posan2',
+            name: 'Possums Strings',
             coordinates: [
                [51.510, -0.18], // mouth
 
             ],
-            videoUrl: '/case-study-in-radio-communication/',
-            imageUrl: '', // Add the URL of the image for this part
-            description: 'posan',
+            videoUrl: '/possum-strings',
+            description: 'Possums gave their string to the Aboriginal people living in the area where they camped. There can be used tomanufature sacred objects to commemorate their visit',
         },
 
         {
@@ -75,8 +68,7 @@
                 [51.498, -0.123], // mouth
 
             ],
-            videoUrl: '/thuwarri-thaa-wilgie-mia-ancient-aboriginal-mining-technology-in-cultural-context/',
-            imageUrl: '', // Add the URL of the image for this part
+            videoUrl: '',
             description: 'spear1',
         },
         ,
@@ -87,29 +79,26 @@
 
             ],
             videoUrl: '/acoustics-of-the-yidaki/',
-            imageUrl: '', // Add the URL of the image for this part
             description: 'spear2',
         },        
 {
-            name: 'M1',
+            name: 'Location',
             coordinates: [
                 [51.509, -0.159], // mouth
 
             ],
-            videoUrl: '/blue-wrens-take-flight-in-engineering-education/',
-            imageUrl: '', // Add the URL of the image for this part
-            description: 'M1',
+            videoUrl: '/',
+            description: 'Locating on map',
         },
         ,
         {
-            name: 'M2',
+            name: 'Relation',
             coordinates: [
                [51.509, -0.095], // mouth
 
             ],
-            videoUrl: '/visions-of-a-bushman-the-community-led-participatory-design-of-an-olkola-cultural-knowledge-centre-as-a-research-through-design-project/',
-            imageUrl: '', // Add the URL of the image for this part
-            description: 'M2',
+            videoUrl: '',
+            description: 'Relation to county',
         },
         {
             name: 'lagoon',
@@ -118,7 +107,6 @@
 
             ],
             videoUrl: 'https://gymbeam.com/blog/cashew-nuts-everything-you-need-to-know/',
-            imageUrl: 'https://gymbeam.com/blog/wp-content/uploads/fly-images/140329/kesu-orechy-clanok_1_-min-437x279.png', // Add the URL of the image for this part
             description: 'Cashew nuts have positive effects on our body across our entire body. Despite the fact that half of their composition is fat, they help us to lose weight. At the same time, they prevent the formation of gallstones and scientific studies have confirmed that they are an effective fighter against digestive tract cancer. Learn why cashews can rightly be called a superfood..',
         },
 
@@ -138,7 +126,6 @@
                                     '<div id="bodyContent">' +
                                     '<p style="font-size:130%;">' + part.description + '</p>' +
                                     '<a href="' + part.videoUrl + '">' +
-                                     "<img src='" + part.imageUrl + "' width='200' height='150'> </a>" +
                                     '<p>Link: <a href="' + part.videoUrl + '">'+ part.videoUrl +
                                     "</p>" +
                                     "</div>" +
